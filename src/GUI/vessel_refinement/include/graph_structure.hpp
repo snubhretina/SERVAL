8e990651015e0be0a0bd291b3e01fe7fb83557c5
snubhretina <64057617+snubhretina@users.noreply.github.com>
2025-09-16 18:36:44 +0900

    Add files via upload

---
+++ src/GUI/vessel_refinement/include/graph_structure.hpp
@@ -0,0 +1,98 @@
+#ifndef _graph_structure
+#define _graph_structure
+
+#include "opencv2/opencv.hpp"
+#include <vector>
+
+//-----------------------------------------------------------------------
+//--------------------------   구조체   ---------------------------------
+//-----------------------------------------------------------------------
+
+class vnode {
+protected:
+	int ID_num;
+	std::vector<vnode*> head, tail;
+
+public:
+	//getter
+	int get_ID();
+	std::vector<vnode*>& get_head();
+	std::vector<vnode*>& get_tail();
+
+	void set_ID(int num);
+
+	//method
+	void change_head_tail();		//트리 구조에서 노드의 방향성을 바꾸는 함수 head쪽이 optical_disk와 가까운 쪽
+	void push_head(vnode& target);	//head쪽에 대상 노드를 연결
+	void push_tail(vnode& target);	//tail쪽에 대상 노드를 연결 -> graph 클래스에서 두개를 합치는 메소드 생성 해야함
+};
+
+class vbranch : public vnode {
+private:
+	std::vector<std::vector<double>> branch_poly_x;
+	std::vector<std::vector<double>> branch_poly_y;
+	std::vector<std::vector<double>> branch_poly_r;
+	std::vector<cv::Point> end_points; //그래프에서 바이퍼랑 연결 할 때 필요한 코드
+
+public:
+	//getter
+	std::vector<std::vector<double>> get_poly_x();
+	std::vector<std::vector<double>> get_poly_y();
+	std::vector<std::vector<double>> get_poly_r();
+	std::vector<cv::Point> get_end_points();
+
+	//setter
+	void set_poly_x(const std::vector<std::vector<double>>& poly_x);
+	void set_poly_y(const std::vector<std::vector<double>>& poly_y);
+	void set_poly_r(const std::vector<std::vector<double>>& poly_r);
+	void set_end_points(cv::Point pt1, cv::Point pt2);
+};
+
+class vbifur : public vnode {
+private:
+	cv::Point center_coor;
+	cv::Mat vbifur_mask;
+	std::vector<cv::Point> bifur_edge;
+
+public:
+	//getter
+	cv::Point& get_center_coor();
+	cv::Mat& get_vbifur_mask();
+	std::vector<cv::Point>& get_bifur_edge();
+
+	//setter
+	void set_center_coor(cv::Point& center);
+	void set_vbifur_mask(cv::Mat& mask);
+	void set_bifur_edge(std::vector<cv::Point> edge);
+};
+
+//-----------------------------------------------------------------------
+//--------------------------    구조    ---------------------------------
+//-----------------------------------------------------------------------
+class vgraph {
+private:
+	std::vector <vbranch> vbranches;
+	std::vector <vbifur> vbifurs;
+public:
+	std::vector<vbranch> get_branch();
+	std::vector<vbifur> get_bifur();
+	void add_bifur(vbifur new_bifur);
+	void add_branch(vbranch new_branch);
+	vbifur& find_bifur(cv::Point center); //중심 좌표를 입력하면, 해당하는 bifur을 가져오는 함수
+	void connect();
+};
+
+// 나중에 그래프 완성 후 수정 작업
+class vtree {
+private:
+	vnode* root;
+	std::vector <vbranch> vbranches;
+	std::vector <vbifur> vbifurs;
+public:
+};
+
+cv::Mat padImage(const cv::Mat& inputImage, int size);
+cv::Mat dilateImage(const cv::Mat& inputImage, int kernelSize);
+std::vector<cv::Point> where(const cv::Mat& image);
+bool isPointInVector(const std::vector<cv::Point>& points, const cv::Point& targetPoint);
+#endif
---
+++ src/GUI/vessel_refinement/include/vascularity.hpp
@@ -0,0 +1,47 @@
+#ifndef _vascularity
+#define _vascularity
+
+#include <vector>
+#include <string>
+
+#include "opencv2/opencv.hpp"
+#include "graph_structure.hpp"
+
+class vascularity {
+private:
+    //images
+    cv::Mat fundus;    // 안구 이미지
+    cv::Mat mask;      // 안구 마스크
+    cv::Mat skel;      // 스켈레톤 이미지
+    cv::Mat fa;        // 안구 조영 사진(optional)
+
+    //optic disk
+    cv::Point optic_disk_center; //optic disk 중심 좌표
+    int optic_disk_r; // optic disk 반지름
+
+    //vector 구조
+    vgraph a_graph; //동맥 그래프 구조 r            
+    vgraph v_graph; //정맥 그래프 구조 b
+
+    std::vector<vtree*> a_tree; //동맥 트리 구조                  
+    std::vector<vtree*> v_tree; //정맥 트리 구조   
+
+    //기타 자료
+    cv::Mat node_map;  // gui에서 어떤 대상으로 매핑해줄껀지 가이드 해주는 마스크 
+
+public:
+    //생성자
+    vascularity(cv::Mat img, cv::Mat vmask);
+
+    //vecterization
+    void make_graph();
+    void simple_vectorization(); //연결성이 없음 구 버전
+
+    //기타 메소드
+    void where(const cv::Mat& skel, std::vector<cv::Point> &result);
+    bool write(std::string path = "result/vector_json/recent_result.json");
+    void visualize(int sampling_dis = 1,bool save = false);
+    void visualize_graph();
+};
+
+#endif
